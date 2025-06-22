7bebcf7de0165b916261c868c9b686ef5d9d263c
= <=>
2025-06-22 15:44:09 +0530

    feat: add filter button to ProductList for sidebar toggle functionality

--- src/app/components/product/list/ProductList.tsx
+++ src/app/components/product/list/ProductList.tsx
@@ -1,25 +1,32 @@
-"use client"
+"use client";
 
 import React from "react";
 import ProductCard from "./../card/ProductCard";
 import styles from "./product.module.css";
 import { Product } from "@/app/types/product";
 import ProductCardSkeleton from "../../skeleton/product/ProductCard";
+import { useAppContext } from "@/app/context/AppContext";
 
 interface Props {
   products: Product[];
 }
 
 function ProductList({ products }: Props) {
+  const { toggleSidebar } = useAppContext();
   return (
-    <div className={styles.grid}>
-      {!products.length ? (
-        <ProductCardSkeleton />
-      ) : (
-        products.map((product: Product) => (
-          <ProductCard key={product.id} product={product} />
-        ))
-      )}
+    <div>
+      <button className={styles.filterToggleBtn} onClick={toggleSidebar}>
+        FILTER
+      </button>
+      <div className={styles.grid}>
+        {!products.length ? (
+          [1,2,3,4,5].map(ele=><ProductCardSkeleton />)
+        ) : (
+          products.map((product: Product) => (
+            <ProductCard key={product.id} product={product} />
+          ))
+        )}
+      </div>
     </div>
   );
 }
